b8e86e19b04a4b861a58e1cc01eaa519d2875470
Sakib2601 <sakibharsolia2@gmail.com>
2021-09-11 22:19:18 +0530

    Validation in Login Form

--- app/src/main/java/com/example/cryptwatch/LoginInfo/Login.java
+++ app/src/main/java/com/example/cryptwatch/LoginInfo/Login.java
@@ -3,6 +3,9 @@ package com.example.cryptwatch.LoginInfo;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,7 +25,8 @@ public class Login extends AppCompatActivity {
 
     ImageView imageView;
     TextView slogan_name , login;
-    TextInputLayout username , password;
+    EditText username , password;
+    TextInputLayout tilusername , tilpassword;
     Button btn_skip , btn_logtoreg , btn_login;
 
     @Override
@@ -35,35 +39,103 @@ public class Login extends AppCompatActivity {
         imageView  =(ImageView) findViewById(R.id.img_login);
         slogan_name = (TextView) findViewById(R.id.slogan_name);
         login = (TextView) findViewById(R.id.login);
-        username = (TextInputLayout) findViewById(R.id.log_username);
-        password = (TextInputLayout) findViewById(R.id.log_password);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        tilusername = (TextInputLayout) findViewById(R.id.log_username);
+        tilpassword = (TextInputLayout) findViewById(R.id.log_password);
+
         btn_login = (Button) findViewById(R.id.btnlogin);
         btn_skip = (Button) findViewById(R.id.btn_skip);
         btn_logtoreg = (Button) findViewById(R.id.btn_logtoreg);
 
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
 
+       skip();
+       logToReg();
+    }
+    private void login(){
+
+        String uname = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(TextUtils.isEmpty(uname)){
+            username.requestFocus();
+            tilusername.setErrorEnabled(true);
+            tilusername.setError("Enter Username");
+
+            tilusername.getEditText().addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    tilusername.setError(null);
+                    tilusername.setErrorEnabled(false);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+        }
+        else if(TextUtils.isEmpty(pass)) {
+            password.requestFocus();
+            tilpassword.setErrorEnabled(true);
+            tilpassword.setError("Enter Password");
+
+            tilpassword.getEditText().addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    tilpassword.setError(null);
+                    tilpassword.setErrorEnabled(false);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+        }
+        else{
+
+        }
+    }
+
+    private void skip(){
         btn_skip.setOnClickListener(v -> {
             Intent intent = new Intent(Login.this, DashBoard.class);
             startActivity(intent);
         });
+    }
 
-
+    private void logToReg(){
         btn_logtoreg.setOnClickListener(v -> {
             Intent intent = new Intent(Login.this, Register.class);
-
 //            Pair[] pairs = new Pair[7];
-//
 //            pairs[0] = new Pair<View,String>(imageView , "logo_image");
 //            pairs[1] = new Pair<View,String>(slogan_name , "logo_text");
 //            pairs[2] = new Pair<View,String>(login , "tran_signUpText");
-//            pairs[3] = new Pair<View,String>(username , "tran_username");
-//            pairs[4] = new Pair<View,String>(password , "tran_password");
+//            pairs[3] = new Pair<View,String>(tilusername , "tran_username");
+//            pairs[4] = new Pair<View,String>(tilpassword , "tran_password");
 //            pairs[5] = new Pair<View,String>(btn_login , "tran_btnlogin");
 //            pairs[6] = new Pair<View,String>(btn_logtoreg , "tran_logtoreg");
-//
 //            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this,pairs);
 //            startActivity(intent,options.toBundle());
-              startActivity(intent);
+            startActivity(intent);
         });
     }
 }
