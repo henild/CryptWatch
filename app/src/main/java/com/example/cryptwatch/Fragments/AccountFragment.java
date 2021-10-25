bfd16b1839464bfaa4465c88d9463cc15b23ff8c
Sakib2601 <sakibharsolia2@gmail.com>
2021-10-25 23:35:46 +0530

    Logout Button in fragment_account.xml And tried to decrease size of Login page haven't tested it yet

--- app/src/main/java/com/example/cryptwatch/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/cryptwatch/Fragments/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.cryptwatch.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.cryptwatch.LoginInfo.Login;
 import com.example.cryptwatch.R;
 
 
 public class AccountFragment extends Fragment {
 
+    Button logout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account,container,false);
+        logout = (Button) view.findViewById(R.id.btnlogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
