ba20cb6a8e884b059c197a5beda23058bb00b99d
Sakib2601 <sakibharsolia2@gmail.com>
2021-10-28 15:29:44 +0530

    Adapter class and holder comlpeted

--- app/src/main/java/com/example/cryptwatch/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/cryptwatch/Fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.cryptwatch.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -12,7 +13,11 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.cryptwatch.Adapter.CurrencyAdapter;
+import com.example.cryptwatch.CurrencyRVModel;
 import com.example.cryptwatch.R;
+
+import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment {
@@ -20,6 +25,8 @@ public class HomeFragment extends Fragment {
     EditText searchbar;
     RecyclerView recyclerView;
     ProgressBar progressBar;
+    ArrayList<CurrencyRVModel> currencyRVModelArrayList;
+    CurrencyAdapter currencyAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,7 +37,13 @@ public class HomeFragment extends Fragment {
         searchbar = (EditText) view.findViewById(R.id.searchbar);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
         progressBar = (ProgressBar) view.findViewById(R.id.progressbar);
-
+        currencyRVModelArrayList = new ArrayList<>();
+        currencyAdapter = new CurrencyAdapter(currencyRVModelArrayList,getActivity());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(currencyAdapter);
         return view;
+    }
+
+    private void setCurrencyData(){
     }
 }
