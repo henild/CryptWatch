c697be4b43f16d262707d88d170a7d329c538260
henil <dedaniahenil@gmail.com>
2021-10-28 16:52:11 +0530

    Connect Data with UI

--- app/src/main/java/com/example/cryptwatch/Adapter/CurrencyRVAdapter.java
+++ app/src/main/java/com/example/cryptwatch/Adapter/CurrencyRVAdapter.java
@@ -1,6 +1,5 @@
 package com.example.cryptwatch.Adapter;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,31 +15,36 @@ import com.example.cryptwatch.R;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class CurrencyAdapter extends RecyclerView.Adapter<CurrencyAdapter.Viewholder> {
+public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Viewholder> {
 
     private ArrayList<CurrencyRVModel> currencyRVModelArrayList;
     private Context context;
     private static DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
-    public CurrencyAdapter(ArrayList<CurrencyRVModel> currencyRVModelArrayList, Context context) {
+    public CurrencyRVAdapter(ArrayList<CurrencyRVModel> currencyRVModelArrayList, Context context) {
         this.currencyRVModelArrayList = currencyRVModelArrayList;
         this.context = context;
     }
 
+    public void filteredList(ArrayList<CurrencyRVModel> filteredList) {
+        currencyRVModelArrayList = filteredList;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
-    public CurrencyAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CurrencyRVAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.row,parent,false);
-        return new CurrencyAdapter.Viewholder(view);
+        return new CurrencyRVAdapter.Viewholder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CurrencyAdapter.Viewholder holder, int position) {
+    public void onBindViewHolder(@NonNull CurrencyRVAdapter.Viewholder holder, int position) {
         CurrencyRVModel currencyRVModel = currencyRVModelArrayList.get(position);
         holder.currencyName.setText(currencyRVModel.getCurrencyName());
         holder.currencySymbol.setText(currencyRVModel.getCurrencySymbol());
         holder.currencyRate.setText("₹ "+decimalFormat.format(currencyRVModel.getPrice()));
-        holder.currencyChangePercentage.setText(currencyRVModel.getPriceChangeIn24Hr()+" %");
+        holder.currencyChangePercentage.setText(decimalFormat.format(currencyRVModel.getPriceChangeIn24Hr())+" %");
     }
 
     @Override
--- app/src/main/java/com/example/cryptwatch/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/cryptwatch/Fragments/HomeFragment.java
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,18 +20,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.cryptwatch.CurrencyRVModel;
 import com.example.cryptwatch.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.example.cryptwatch.Adapter.CurrencyAdapter;
-import com.example.cryptwatch.CurrencyRVModel;
-import com.example.cryptwatch.R;
+import com.example.cryptwatch.Adapter.CurrencyRVAdapter;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
 public class HomeFragment extends Fragment {
@@ -37,8 +42,7 @@ public class HomeFragment extends Fragment {
     EditText searchbar;
     RecyclerView recyclerView;
     ProgressBar progressBar;
-    ArrayList<CurrencyRVModel> currencyRVModelArrayList;
-    CurrencyAdapter currencyAdapter;
+    CurrencyRVAdapter currencyRVAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,13 +54,42 @@ public class HomeFragment extends Fragment {
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
         progressBar = (ProgressBar) view.findViewById(R.id.progressbar);
         currencyRVModelArrayList = new ArrayList<>();
-        currencyAdapter = new CurrencyAdapter(currencyRVModelArrayList,getActivity());
+        currencyRVAdapter = new CurrencyRVAdapter(currencyRVModelArrayList,getActivity());
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setAdapter(currencyAdapter);
+        recyclerView.setAdapter(currencyRVAdapter);
+        getCurrencyData();
+        searchbar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filterCurrencies(s.toString());
+
+            }
+        });
         return view;
     }
 
-    private void setCurrencyData(){
+    private void filterCurrencies(String query) {
+        ArrayList<CurrencyRVModel> filteredList = new ArrayList<>();
+        for(CurrencyRVModel item: currencyRVModelArrayList) {
+            if(item.getCurrencyName().toLowerCase(Locale.ROOT).contains(query.toLowerCase(Locale.ROOT))){
+                filteredList.add(item);
+            }
+        }
+        if(filteredList.isEmpty()) {
+            Toast.makeText(getActivity(), "No currency found for this search query", Toast.LENGTH_SHORT).show();
+        } else {
+            currencyRVAdapter.filteredList(filteredList);
+        }
     }
 
     private void getCurrencyData() {
@@ -65,19 +98,32 @@ public class HomeFragment extends Fragment {
         String url = base_url + param_url;
 
         RequestQueue reqQueue = Volley.newRequestQueue(getActivity());
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onResponse(JSONArray dataArray) {
+                try {
+                    for(int i = 0; i < dataArray.length(); i++) {
+                        JSONObject dataObj = dataArray.getJSONObject(i);
+                        String currencyName = dataObj.getString("name");
+                        String currencySymbol = dataObj.getString("symbol");
+                        double price = dataObj.getDouble("current_price");
+                        double priceChangeIn24Hr = dataObj.getDouble("price_change_percentage_24h");
+                        currencyRVModelArrayList.add(new CurrencyRVModel(currencyName, currencySymbol, price, priceChangeIn24Hr));
+                        Log.d("DEBUG", currencyRVModelArrayList.toString());
+                    }
+                    currencyRVAdapter.notifyDataSetChanged();
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Fail to parse JSON data", Toast.LENGTH_SHORT).show();
+                }
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                Log.d("DEBUG", error.toString());
             }
         });
-
-        reqQueue.add(jsonObjectRequest);
-
+        reqQueue.add(jsonArrayRequest);
     }
 }
