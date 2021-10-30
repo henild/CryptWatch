07d717d5510b275a2dcb55311eef987eb86e0766
Sakib2601 <sakibharsolia2@gmail.com>
2021-10-30 19:37:00 +0530

    Search View change card theek kiya thoda favourite wala ek baar check karna na

--- app/src/main/java/com/example/cryptwatch/Adapter/CurrencyRVAdapter.java
+++ app/src/main/java/com/example/cryptwatch/Adapter/CurrencyRVAdapter.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cryptwatch.CurrencyRVModel;
@@ -18,6 +21,7 @@ import java.util.ArrayList;
 public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Viewholder> {
 
     private ArrayList<CurrencyRVModel> currencyRVModelArrayList;
+    ToggleButton favbutton;
     private Context context;
     private static DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
@@ -45,6 +49,18 @@ public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Vi
         holder.currencySymbol.setText(currencyRVModel.getCurrencySymbol());
         holder.currencyRate.setText("₹ "+decimalFormat.format(currencyRVModel.getPrice()));
         holder.currencyChangePercentage.setText(decimalFormat.format(currencyRVModel.getPriceChangeIn24Hr())+" %");
+        holder.favbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    favbutton.setBackgroundDrawable(ContextCompat.getDrawable(context.getApplicationContext(),R.drawable.ic_card_favourite));
+                }
+                else {
+                    favbutton.setBackgroundDrawable(ContextCompat.getDrawable(context.getApplicationContext(),R.drawable.ic_card_not_favourite));
+                }
+            }
+        });
+
     }
 
     @Override
@@ -53,13 +69,18 @@ public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.Vi
     }
 
     public class Viewholder extends RecyclerView.ViewHolder {
+
         private TextView currencyName , currencySymbol , currencyRate , currencyChangePercentage;
+        ToggleButton favbutton;
+
         public Viewholder(@NonNull View itemView) {
             super(itemView);
             currencyName = itemView.findViewById(R.id.card_name);
             currencySymbol = itemView.findViewById(R.id.card_symbol);
             currencyRate = itemView.findViewById(R.id.card_price);
             currencyChangePercentage = itemView.findViewById(R.id.card_percentage);
+            favbutton = itemView.findViewById(R.id.button_fav);
+            favbutton.setChecked(false);
         }
     }
 }
