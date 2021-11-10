52637c1d3cade1303645e81177e4aed43d1e4e02
Sakib2601 <sakibharsolia2@gmail.com>
2021-11-10 21:32:31 +0530

    account fragment

--- app/src/main/java/com/example/cryptwatch/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/cryptwatch/Fragments/AccountFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +23,7 @@ import java.util.ArrayList;
 
 public class AccountFragment extends Fragment {
     Button logout;
-    TextView username, email;
+    EditText username, email;
     private FirebaseAuth mAuth;
 
     @Override
